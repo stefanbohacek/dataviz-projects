670f0faf13216216b4ca0786142bb0eab1ac7e65
Stefan Bohacek <stefan@fourtonfish.com>
2024-03-06 14:58:53 -0500

    Cleanup.

--- src/js/main.js
+++ src/js/main.js
@@ -15,4 +15,11 @@ ready(() => {
       }
     }
   }
+
+  setTimeout(() => {
+    if (!window.Matomo) {
+      const url = `https://tools.stefanbohacek.dev/info?idsite=1&action_name=${encodeURIComponent(document.title)}&url=${window.location.href}&uadata=${encodeURIComponent(JSON.stringify(navigator.userAgentData))}&res=${window.screen.availWidth}x${window.screen.availHeight}`;
+      fetch(url);
+    }
+  }, 2000);  
 });
