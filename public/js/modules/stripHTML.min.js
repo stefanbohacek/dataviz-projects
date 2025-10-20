67bb5a25c4b2eb18708a989e8505e51420866e40
Stefan Bohacek <stefan@fourtonfish.com>
2025-10-19 22:16:39 -0400

    Cleanup.

--- public/js/modules/stripHTML.min.js
+++ public/js/modules/stripHTML.min.js
@@ -1,7 +1,14 @@
 const stripHTML = (html) => {
-  let tmp = document.createElement("DIV");
-  tmp.innerHTML = html;
-  return tmp.textContent || tmp.innerText || "";
+  if (!html || typeof html !== "string") {
+    return "";
+  }
+  const tmp = document.createElement("template");
+  const spacedHtml = html
+    .replace(/<\/?(div|p|br|h[1-6]|li|tr|td|th)[^>]*>/gi, " $& ")
+    .replace(/<\/a>/gi, " </a>");
+
+  tmp.innerHTML = spacedHtml.trim();
+  return tmp.content.textContent.replace(/\s+/g, " ").trim();
 };
 
 export default stripHTML;
