ef884835541558754385c46ac15e0fe2a9cb4e43
Stefan Bohacek <stefan@fourtonfish.com>
2026-03-26 11:20:13 -0400

    Added field for customizing server for opening profile links.

--- projects/fediverse-accounts/src/js/main.js
+++ projects/fediverse-accounts/src/js/main.js
@@ -2,28 +2,44 @@
 
 import ready from "/js/modules/ready.min.js";
 import sortArrayOfObjects from "/js/modules/sortArrayOfObjects.min.js";
+import getUrlParams from "/js/modules/getUrlParams.min.js";
 
 ready(async () => {
+  // const query = `
+  // SELECT DISTINCT ?org ?orgLabel ?orgTypeLabel ?hostAt ?MastName WHERE {
+  //   SERVICE wikibase:label {
+  //     bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en".
+  //   }
+  //   ?org wdt:P4033
+  //   ?MastName.
+  //   ?org wdt:P31
+  //   ?orgType.
+  //   BIND (strafter(?MastName,"@") AS ?hostAt). } ORDER BY ?hostAt LIMIT 20000
+  // `;
 
-  const query = `
-  SELECT DISTINCT ?org ?orgLabel ?orgTypeLabel ?hostAt ?MastName WHERE {
-    SERVICE wikibase:label {
-      bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en".
-    }
-    ?org wdt:P4033
-    ?MastName.
-    ?org wdt:P31
-    ?orgType.
-    BIND (strafter(?MastName,"@") AS ?hostAt). } ORDER BY ?hostAt LIMIT 20000
-  `;
-  
   // const resp = await fetch(
   //   `https://tools.stefanbohacek.com/wikidata/?query=${encodeURIComponent(query)}&token=Lyvgzjrheuk3dxwm8wgvpexndj5nhy8x7xihyt9ltmhknwbj1xnwpc22svoqvr`
   // );
 
+  const params = getUrlParams(false);
+  const serverParam = params.server || "mastodon.social";
+  const server = `https://${serverParam}`;
+
+  const serverInputField = document.getElementById("fediverse-server");
+
+  document.getElementById("server-submit").addEventListener("click", () => {
+    const val = serverInputField.value.trim().replace(/^https?:\/\//, "");
+    if (val) {
+      const url = new URL(window.location.href);
+      url.searchParams.set("server", val);
+      window.location.href = url.toString();
+    }
+  });
+
+  serverInputField.value = serverParam;
 
   const resp = await fetch(
-    "https://tools.stefanbohacek.com/wikidata/?query=SELECT%20DISTINCT%20%3Forg%20%3ForgLabel%20%3ForgTypeLabel%20%3FhostAt%20%3FMastName%20WHERE%20%7B%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cen%22.%20%7D%20%3Forg%20wdt%3AP4033%20%3FMastName.%20%3Forg%20wdt%3AP31%20%3ForgType.%20BIND%20(strafter(%3FMastName%2C%22%40%22)%20AS%20%3FhostAt).%20%7D%20ORDER%20BY%20%3FhostAt%20LIMIT%2020000&token=lYVGZjrHEUk3DxWm8WgVpExNdj5NHY8x7XiHyt9ltMhKNwbJ1XNwPC22SvoqVR"
+    "https://tools.stefanbohacek.com/wikidata/?query=SELECT%20DISTINCT%20%3Forg%20%3ForgLabel%20%3ForgTypeLabel%20%3FhostAt%20%3FMastName%20WHERE%20%7B%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cen%22.%20%7D%20%3Forg%20wdt%3AP4033%20%3FMastName.%20%3Forg%20wdt%3AP31%20%3ForgType.%20BIND%20(strafter(%3FMastName%2C%22%40%22)%20AS%20%3FhostAt).%20%7D%20ORDER%20BY%20%3FhostAt%20LIMIT%2020000&token=lYVGZjrHEUk3DxWm8WgVpExNdj5NHY8x7XiHyt9ltMhKNwbJ1XNwPC22SvoqVR",
   );
 
   const fediverseAccounts = await resp.json();
@@ -46,20 +62,19 @@ ready(async () => {
       field: "type",
       editor: "input",
       sorter: "string",
-      headerFilter: "input"
+      headerFilter: "input",
     },
     {
       title: "Account",
       field: "account",
       sorter: "string",
       headerFilter: "input",
-      // TODO: The account link format depends on the platform used, eg. Mastodon usernames start with @, on Pleroma they do not.
-      // formatter: "link",
-      // formatterParams: {
-      //   labelField: "account",
-      //   urlField: "account_url",
-      //   target: "_blank",
-      // },
+      formatter: "link",
+      formatterParams: {
+        labelField: "account",
+        urlField: "account_url",
+        target: "_blank",
+      },
     },
     {
       title: "Server",
@@ -71,7 +86,7 @@ ready(async () => {
       //   valuesLookup: true,
       //   sortValuesList: "asc",
       //   clearable: true,
-      // },      
+      // },
       formatter: "link",
       formatterParams: {
         labelField: "server",
@@ -85,13 +100,11 @@ ready(async () => {
 
   fediverseAccounts.forEach((datapoint, index) => {
     const account = `@${datapoint?.MastName?.value}`.replace("@@", "@");
-    if (
-      !data
-        .map((acc) => acc.account)
-        .includes(account)
-    ) {
-      const server = datapoint?.hostAt?.value;
-      const username = datapoint?.MastName?.value.replace(`@${server}`, "");
+    if (!data.map((acc) => acc.account).includes(account)) {
+      // const server = datapoint?.hostAt?.value;
+      // const username = datapoint?.MastName?.value.replace(`@${server}`, "");
+      const remoteServer = datapoint?.hostAt?.value;
+      const username = datapoint?.MastName?.value.replace(`@${remoteServer}`, "");
 
       data.push({
         id: index,
@@ -99,9 +112,12 @@ ready(async () => {
         url: datapoint?.org?.value,
         type: datapoint?.orgTypeLabel?.value,
         account,
-        account_url: `https://${server}/@${username}`,
-        server_url: `https://${server}`,
-        server,
+        // account_url: `https://${server}/@${username}`,
+        // server_url: `https://${server}`,
+        // server,
+        account_url: `${server}/@${username}@${remoteServer}`,
+        server_url: `https://${remoteServer}`,
+        server: remoteServer,
       });
     }
   });
